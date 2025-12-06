4a679f7a2012b2f930e09aa67744f040680c694e
Paulo Aguiar (Upstart Software LLC) <v-paguiar@microsoft.com>
2025-12-06 15:49:22 -0300

    feat:all project

--- create_template.py
+++ create_template.py
@@ -24,7 +24,6 @@ door_block.add_line((0.05, 0), (0, 0))
 # Arc to show opening
 door_block.add_arc(center=(0,0), radius=0.8, start_angle=0, end_angle=90)
 
-<<<<<<< HEAD
 # Door 70cm
 door_block_70 = doc.blocks.new(name="porta_70", base_point=(0, 0))
 door_block_70.add_line((0, 0), (0, 0.7))
@@ -41,8 +40,6 @@ door_block_90.add_line((0.05, 0.9), (0.05, 0))
 door_block_90.add_line((0.05, 0), (0, 0))
 door_block_90.add_arc(center=(0,0), radius=0.9, start_angle=0, end_angle=90)
 
-=======
->>>>>>> 56080d7ad81cd7142cc5d6f7c0eab56befadc55d
 
 # Define a simple block for a window
 window_block = doc.blocks.new(name="janela_150", base_point=(0, 0))
@@ -50,7 +47,6 @@ window_block.add_line((0, -0.075), (0, 0.075))
 window_block.add_line((1.5, -0.075), (1.5, 0.075))
 window_block.add_line((0, 0), (1.5, 0))
 
-<<<<<<< HEAD
 # Window 100cm
 window_block_100 = doc.blocks.new(name="janela_100", base_point=(0, 0))
 window_block_100.add_line((0, -0.075), (0, 0.075))
@@ -63,8 +59,6 @@ window_block_200.add_line((0, -0.075), (0, 0.075))
 window_block_200.add_line((2.0, -0.075), (2.0, 0.075))
 window_block_200.add_line((0, 0), (2.0, 0))
 
-=======
->>>>>>> 56080d7ad81cd7142cc5d6f7c0eab56befadc55d
 
 # Define a simple block for a socket
 socket_block = doc.blocks.new(name="tomada_dupla", base_point=(0, 0))
